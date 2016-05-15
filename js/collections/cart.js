e9106753239c59bd6f5f48e80324edff33831098
hqcasanova <hqcasanova@users.noreply.github.com>
2016-05-15 23:23:20 +0100

    Fixes overlooked case where 2for1 items are selectively removed leaving some free items that should have a price

--- js/collections/cart.js
+++ js/collections/cart.js
@@ -19,26 +19,49 @@ Micado.Collections.Cart = Micado.Collections.Entities.extend({
             var discountCode = model.get('discountCode');
 
             discountCode && this[discountCode + suffix](model);
-            this.trigger('pricing:update');     //prevents calculation of total get ahead of pricing
+            this.trigger('pricing:update');     //prevents calculation of total getting ahead of pricing
         }
     },
 
     //If an even number of 2for1 items exist, make last one added free  
     off1: function (model) {
-        var numItems2for1 = this.where({discountCode: 'off1'}).length;
+        var numItems2for1 = this.where({discountCode: 'off1', code: model.get('code')}).length;
 
         if ((numItems2for1 > 1) && (numItems2for1 % 2 == 0)) {
             model.set('price', 0);
         }
     },
 
-    //Removal of 2for1 items doesn't have any effect on existing prices
-    off1Rev: function () {},
+    //Makes sure all free items are paired with a priced one whenever possible
+    off1Rev: function (model) {
+        var attrs2for1 = {discountCode: 'off1', code: model.get('code')};
+        var items2for1 = this.where(attrs2for1);
+        var numItems2for1 = items2for1.length;
+        var freeItems;
+        var numFreeItems;
+        var balanceRatio;
+
+        //The collection may fall 'out of balance' when it contains 3 or more items
+        if (numItems2for1 > 2) {
+            freeItems = this.where(_.extend({}, attrs2for1, {price: 0}));
+            numFreeItems = freeItems.length;
+            balanceRatio = numFreeItems - numItems2for1 / 2;
+
+            //There are 2 free items more than the number of priced ones => restores price for last free item
+            if (balanceRatio == 1) {
+                freeItems.models[numFreeItems - 1].set('price', model.get('price'));
+
+            //There are 2 priced items more than free items => makes first priced item found free
+            } else if (balanceRatio == -1) {
+                items2for1.find(function (item) {return item.get('price') != 0}).set('price', 0);
+            }    
+        }
+    },
 
     //If 3 or more items are added with reduced pricing, update the price of
     //all items to  that of the original 
     reduced3: function (model) {
-        var itemsReduced = this.where({discountCode: 'reduced3'});
+        var itemsReduced = this.where({discountCode: 'reduced3', code: model.get('code')});
         var numItemsReduced = itemsReduced.length;
         var newPrice = model.get('discountPrice');
 
@@ -57,7 +80,7 @@ Micado.Collections.Cart = Micado.Collections.Entities.extend({
 
     //Pricing varies while removing reduced-priced items only if less than 3 left
     reduced3Rev: function (model) {
-        var itemsReduced = this.where({discountCode: 'reduced3'});
+        var itemsReduced = this.where({discountCode: 'reduced3', code: model.get('code')});
 
         if (itemsReduced.length == 2) {
             itemsReduced.forEach(function (item) {
